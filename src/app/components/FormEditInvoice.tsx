4c72fbc092f9fdb308ff510d9f32ff10f796a8d3
Jordi Verdaguer <jverdaguervilaro@gmail.com>
2024-10-21 16:51:20 +0200

    Fixed Error in LocalUrl

--- src/app/components/FormEditInvoice.tsx
+++ src/app/components/FormEditInvoice.tsx
@@ -32,7 +32,7 @@ export default function FormEditInvoice({ invoice }: FormEditInvoiceProps) {
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
-    const res = await fetch("http://localhost:3000/api/invoice/edit", {
+    const res = await fetch("/api/invoice/edit", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
